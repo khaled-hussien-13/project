9228ee774a972dc98e49f76e54f7a997ced53c98
khaled-hussien-13 <132480714+khaled-hussien-13@users.noreply.github.com>
2023-05-03 19:20:32 +0300

    Add files via upload

---
+++ .github/workflows/install.sh
@@ -0,0 +1,39 @@
+termux-setup-storage
+clear
+pkg update 
+clear
+pkg install sox
+clear
+pkg install figlet
+clear
+pkg install python python2
+clear
+pkg install python3
+clear
+pkg install git
+clear
+pkg install wget
+clear
+pkg install php
+clear
+pkg install proot-distro
+clear
+proot-distro install ubuntu
+clear
+pkg install curl
+clear
+pkg install openssh
+clear
+pip install gtts
+clear
+pip install requests
+clear
+pip install pytube
+clear
+pip install compileall
+clear
+pip install scapy
+clear
+pip install django
+clear
+
